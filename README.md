ff6af4531c5f68b0300fc7e94e95369e2bc1adb2
Gizmo Head <benjamin.potzmann@outlook.com>
2021-12-05 03:21:00 +0100

    adds unit tests

---
+++ README.md
@@ -0,0 +1,5 @@
+
+to start: 
+`cargo run --bin <day_binary> "<path-to-input>"`
+
+
--- day_4/main.rs
+++ day_4/main.rs
@@ -9,6 +9,43 @@ struct Board {
     chosen: Vec<Vec<bool>>
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_INPUT: &str = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"#;
+
+    #[test]
+    fn test_day4_task1() {
+        let (chosen_numbers, boards) = parse_input(TEST_INPUT);
+        assert_eq!(task1(&chosen_numbers, boards), 4512);
+    }
+
+    #[test]
+    fn test_day4_task2() {
+        let (chosen_numbers, boards) = parse_input(TEST_INPUT);
+        assert_eq!(task2(&chosen_numbers, boards), 1924);
+    }
+}
+
 impl Board {
     fn choose(&mut self, number: u32) {
         for (row_index, row) in self.numbers.iter().enumerate() {
@@ -87,11 +124,11 @@ fn main() {
         .expect("Parameter INPUT is missing");
     println!("Path: {}", input_path);
 
-    let contents_raw = fs::read_to_string(input_path)
+    let input_raw = fs::read_to_string(input_path)
         .expect("Something went wrong reading the file");
-    let contents = contents_raw.trim();
+    let input = input_raw.trim();
 
-    let (chosen_numbers, boards) = parse_input(contents);
+    let (chosen_numbers, boards) = parse_input(input);
 
     println!("Task1: {}", task1(&chosen_numbers, boards.clone()));
     println!("Task2: {}", task2(&chosen_numbers, boards));
